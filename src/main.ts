a7e29fd424d15ea34366141be893da206251b4c4
woodlinepro <woodlineprodev@gmail.com>
2026-04-30 08:59:43 +0500

    ok

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { Logger } from "@nestjs/common";
+import { AppModule } from "./app.module";
+import * as dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
 
 async function bootstrap() {
-  const logger = new Logger('Bootstrap');
-  const app = await NestFactory.create(AppModule);
+    const logger = new Logger("Bootstrap");
+    const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
 
-  logger.log(`🚀 Application running on port ${port}`);
-  logger.log(`🤖 Bot is active!`);
+    logger.log(`🚀 Application running on port ${port}`);
+    logger.log(`🤖 Bot is active!`);
 }
 
 bootstrap();
